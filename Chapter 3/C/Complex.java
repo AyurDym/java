e287ee6f26e7eb317777ecdad0a24cc89e64ca31
AyurDym <ayur.max@mail.ru>
2026-03-27 14:43:10 +0800

    Update Complex.java

--- Chapter 3/C/Complex.java
+++ Chapter 3/C/Complex.java
@@ -1,1 +1,33 @@
+public class Complex {
+    double real;
+    double imag;
 
+    // Конструкторы
+    Complex() {
+        this.real = 0;
+        this.imag = 0;
+    }
+
+    Complex(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    // Сложение
+    Complex add(Complex other) {
+        return new Complex(this.real + other.real, this.imag + other.imag);
+    }
+
+    // Вывод
+    public String toString() {
+        if (Math.abs(imag) < 0.001) {
+            return String.format("%.2f", real);
+        } else if (Math.abs(real) < 0.001) {
+            return String.format("%.2fi", imag);
+        } else if (imag > 0) {
+            return String.format("%.2f + %.2fi", real, imag);
+        } else {
+            return String.format("%.2f - %.2fi", real, -imag);
+        }
+    }
+}
