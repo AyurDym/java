6ac8b22808dfb3d763ee0f0614cda8674a451334
AyurDym <ayur.max@mail.ru>
2026-03-27 14:43:31 +0800

    Update Fraction.java

--- Chapter 3/C/Fraction.java
+++ Chapter 3/C/Fraction.java
@@ -1,1 +1,20 @@
+public class Fraction {
+    Complex numerator;   // числитель
+    Complex denominator; // знаменатель
 
+    // Конструкторы
+    Fraction() {
+        this.numerator = new Complex(0, 0);
+        this.denominator = new Complex(1, 0);
+    }
+
+    Fraction(Complex numerator, Complex denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    // Вывод дроби
+    public String toString() {
+        return "(" + numerator + ") / (" + denominator + ")";
+    }
+}
